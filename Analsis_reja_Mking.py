60bc20f224a6d9b8951db7470aeb4314d6e6637d
camilogrey <72422105+camilogrey@users.noreply.github.com>
2023-05-23 07:46:56 -0500

    Add files via upload

---
+++ Analsis_reja_Mking.py
@@ -0,0 +1,51 @@
+import seaborn as sns
+import pandas as pd
+pd.set_option('display.max_columns', None)
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import t
+from scipy.stats import zscore
+
+
+df = pd.read_excel("C:/Users/camil/Documents/COURSES/Diplomado Data Sience/Data Sciences/Own Projects/4. Lattice MKing 3 variables (Rel Betwee 2 Var & Stratify Variable)/Satisfc_Gto_Segmento.xlsx")
+print(df.columns)
+
+adultos=df.loc[df["Segmentación"]=="Adultos"]
+adultos_may=df.loc[df["Segmentación"]=="Adultos mayores"]
+Jovenes=df.loc[df["Segmentación"]=="Jóvenes"]
+
+#1. Lattice Plot in multiple groups
+sns.lmplot(x="Satisfacción", y="Gasto Promedio", col="Segmentación", data=df, height=5)
+plt.show()
+
+
+#2. Lattice Plot joined together
+sns.lmplot(x="Satisfacción", y="Gasto Promedio", hue="Segmentación", data=df, height=5)
+plt.show()
+
+
+
+# 3. Pair plot general para ver (relaciones, tendencias o patrones posibles entre variables, las distribuciones de los datos)
+#Detectar tendencias lineales o no lineales o correlaciones 
+sns.pairplot(df)
+plt.show()
+
+# Calcular coeficientes de correlación
+correlation_matrix = df.corr()
+print("Matriz de correlación:")
+print(correlation_matrix)
+
+
+# Crear pair plot
+sns.pairplot(adultos)
+plt.show()
+
+# Crear pair plot
+sns.pairplot(adultos_may)
+plt.show()
+
+
+# Crear pair plot
+sns.pairplot(Jovenes)
+plt.show()
+
